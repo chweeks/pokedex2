30f0d700ff567bc96b15cc1c13e7c3a74312b52a
chweeks <chrisharryweeks@gmail.com>
2016-01-13 17:24:57 +0000

    fixed api call method

--- js/app.js
+++ js/app.js
@@ -17,20 +17,21 @@ App.IndexRoute = Ember.Route.extend({
     var pokemonArray = [];
 
     pokemonRequest = function(i){
-      // $.getJSON("http://pokeapi.co/api/v1/pokemon/"+i.toString()+"/").then(function(response) {
-      //   pokemonArray.push(response);
-      // });
 
-      pokemonArray.push({name: 'Bulbasaur', pkdx_id: 1, types: [{name:'grass'}, {name:'poison'}]});
-      pokemonArray.push({name: 'Ivysaur', pkdx_id: 2, types: [{name:'grass'}, {name:'water'}]});
-      pokemonArray.push({name: 'Venasaur', pkdx_id: 3, types: [{name:'grass'}, {name:'poison'}]});
-      pokemonArray.push({name: 'Charmander', pkdx_id: 4, types: [{name:'fire'}, {name:'normal'}]});
-      pokemonArray.push({name: 'Charmeleon', pkdx_id: 5, types: [{name:'fire'}, {name:'normal'}]});
-      pokemonArray.push({name: 'Charizard', pkdx_id: 6, types: [{name:'fire'}, {name:'flying'}]});
-      pokemonArray.push({name: 'Pidgey', pkdx_id: 7, types: [{name:'normal'}, {name:'flying'}]});
-      pokemonArray.push({name: 'Pidgiotto', pkdx_id: 8, types: [{name:'normal'}, {name:'flying'}]});
+      return Ember.$.getJSON("http://pokeapi.co/api/v1/pokemon/"+i.toString()+"/").then(function(response) {
+        pokemonArray.pushObject(response);
+      });
 
-      return pokemonArray;
+      // pokemonArray.push({name: 'Bulbasaur', pkdx_id: 1, types: [{name:'grass'}, {name:'poison'}]});
+      // pokemonArray.push({name: 'Ivysaur', pkdx_id: 2, types: [{name:'grass'}, {name:'water'}]});
+      // pokemonArray.push({name: 'Venasaur', pkdx_id: 3, types: [{name:'grass'}, {name:'poison'}]});
+      // pokemonArray.push({name: 'Charmander', pkdx_id: 4, types: [{name:'fire'}, {name:'normal'}]});
+      // pokemonArray.push({name: 'Charmeleon', pkdx_id: 5, types: [{name:'fire'}, {name:'normal'}]});
+      // pokemonArray.push({name: 'Charizard', pkdx_id: 6, types: [{name:'fire'}, {name:'flying'}]});
+      // pokemonArray.push({name: 'Pidgey', pkdx_id: 7, types: [{name:'normal'}, {name:'flying'}]});
+      // pokemonArray.push({name: 'Pidgiotto', pkdx_id: 8, types: [{name:'normal'}, {name:'flying'}]});
+      //
+      // return pokemonArray;
     };
 
     collectPokemon = function(noOfPokemon){
@@ -40,6 +41,6 @@ App.IndexRoute = Ember.Route.extend({
       return pokemonArray
     };
 
-    return collectPokemon(1);
+    return collectPokemon(16);
   }
 });
