3055ee11c97c45209330661e2dd28b7df9fcec0c
chweeks <chrisharryweeks@gmail.com>
2016-01-13 18:04:54 +0000

    allowed dynamic picture loading

--- js/app.js
+++ js/app.js
@@ -16,22 +16,16 @@ App.IndexRoute = Ember.Route.extend({
 
     var pokemonArray = [];
 
+    toThreeDigits = function(string) {
+      return ('000'+string).slice(-3)
+    },
+
     pokemonRequest = function(i){
 
       return Ember.$.getJSON("http://pokeapi.co/api/v1/pokemon/"+i.toString()+"/").then(function(response) {
+        response.pkdx_id = toThreeDigits(response.pkdx_id)
         pokemonArray.pushObject(response);
       });
-
-      // pokemonArray.push({name: 'Bulbasaur', pkdx_id: 1, types: [{name:'grass'}, {name:'poison'}]});
-      // pokemonArray.push({name: 'Ivysaur', pkdx_id: 2, types: [{name:'grass'}, {name:'water'}]});
-      // pokemonArray.push({name: 'Venasaur', pkdx_id: 3, types: [{name:'grass'}, {name:'poison'}]});
-      // pokemonArray.push({name: 'Charmander', pkdx_id: 4, types: [{name:'fire'}, {name:'normal'}]});
-      // pokemonArray.push({name: 'Charmeleon', pkdx_id: 5, types: [{name:'fire'}, {name:'normal'}]});
-      // pokemonArray.push({name: 'Charizard', pkdx_id: 6, types: [{name:'fire'}, {name:'flying'}]});
-      // pokemonArray.push({name: 'Pidgey', pkdx_id: 7, types: [{name:'normal'}, {name:'flying'}]});
-      // pokemonArray.push({name: 'Pidgiotto', pkdx_id: 8, types: [{name:'normal'}, {name:'flying'}]});
-      //
-      // return pokemonArray;
     };
 
     collectPokemon = function(noOfPokemon){
@@ -40,7 +34,6 @@ App.IndexRoute = Ember.Route.extend({
       };
       return pokemonArray
     };
-
     return collectPokemon(16);
   }
 });
