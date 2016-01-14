9d8491198bff407978d5c13ce2fc78b59a08d5fd
chweeks <chrisharryweeks@gmail.com>
2016-01-14 18:02:17 +0000

    added moves and home button link

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-  this.route('index', { path: '/' });
+  this.route("index", { path: "/" });
   this.route("pokemon", { path: "/:pkdx_id" });
 });
 
@@ -22,11 +22,18 @@ App.IndexRoute = Ember.Route.extend({
     var pokemonArray = [];
 
     toThreeDigits = function(pkdx_id) {
-      return ('000'+pkdx_id).slice(-3);
+      return ("000"+pkdx_id).slice(-3);
     },
 
     dividebyTen = function(string) {
       return ((parseInt(string))/10).toString();
+    },
+
+    createMoveGroups = function(movesArray) {
+      return moveGroups = [{moves: movesArray.slice(0,5), colour: "#4A90E2"},
+                           {moves: movesArray.slice(5,10), colour: "#606C78"},
+                           {moves: movesArray.slice(10,15), colour: "#BE93E7"},
+                           {moves: movesArray.slice(15,20), colour: "#A0DA5E"}];
     },
 
     pokemonRequest = function(i){
@@ -34,16 +41,19 @@ App.IndexRoute = Ember.Route.extend({
       return Ember.$.getJSON("http://pokeapi.co/api/v1/pokemon/"+i.toString()+"/").then(function(response) {
         response.pkdx_id = toThreeDigits(response.pkdx_id);
 
-        response.generalStats = [{value: dividebyTen(response.height), name: 'Height'},
-                                 {value: dividebyTen(response.weight), name: 'Weight'},
-                                 {value: 'F/M', name: 'Gender'}]
+        response.generalStats = [{value: dividebyTen(response.height), name: "Height"},
+                                 {value: dividebyTen(response.weight), name: "Weight"},
+                                 {value: "F/M", name: "Gender"}]
 
-        response.baseStats = [{value: response.hp, name: 'HP'},
-                              {value: response.attack, name: 'Attack'},
-                              {value: response.defense, name: 'Defense'},
-                              {value: response.sp_atk, name: 'Special Attack'},
-                              {value: response.sp_def, name: 'Special Defense'},
-                              {value: response.speed, name: 'Speed'}]
+        response.baseStats = [{value: response.hp, name: "HP"},
+                              {value: response.attack, name: "Attack"},
+                              {value: response.defense, name: "Defense"},
+                              {value: response.sp_atk, name: "Special Attack"},
+                              {value: response.sp_def, name: "Special Defense"},
+                              {value: response.speed, name: "Speed"}]
+
+        response.moveGroups = createMoveGroups(response.moves);
+
         pokemonArray.pushObject(response);
       });
     };
